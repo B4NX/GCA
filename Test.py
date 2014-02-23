a448005c22d91c13e11cf943be7381b74908f8bd
Nikko <nikko.rush@gmail.com>
2014-02-22 17:08:49 -0800

    Test Set #2

--- Test.py
+++ Test.py
@@ -5,8 +5,6 @@ import datetime
 
 letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\\', '`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?']
 
-outfile = open("results.csv",'a')
-
 randGen=SystemRandom()
 
 def getRandWord():
@@ -24,17 +22,19 @@ def makeString(charArray:list):
     return mssg
 
 def writeOut(mssg:str, enc, dec, seed, steps):
-    outfile = open("results.csv",'a')
+    outfile = open("results.tsv",'a')
     time=str(datetime.datetime.now())
-    outfile.write(str(mssg)+"\t"+str(enc)+"\t"+str(dec)+"\t"+str(seed)+"\t"+str(steps))
+    print(time+"\t"+str(mssg)+"\t"+str(enc)+"\t"+str(dec)+"\t"+str(seed)+"\t"+str(steps)+"\n")
+    outfile.write(time+"\t"+str(mssg)+"\t"+str(enc)+"\t"+str(dec)+"\t"+str(seed)+"\t"+str(steps)+"\n")
     outfile.close()
 
-for x in range(0,1000):
+for x in range(0,5):
+    print(x)
     mssg=getRandWord()
     seed=CAcrypto.RandSeed(256)
     steps=randGen.randint(75,125)
-    enc=GCA.EncryptMessage(mssg,seed,100)
-    dec=GCA.DecryptMessage(enc,seed,100)
-    writeOut(mssg,enc,dec,seed,100)
+    enc=GCA.EncryptMessage(mssg,seed,steps)
+    dec=GCA.DecryptMessage(enc,seed,steps)
+    writeOut(mssg,enc,dec,seed,steps)
 
 print("All done.")
