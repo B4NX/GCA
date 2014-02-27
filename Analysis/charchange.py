c10a191874c703be4327d2b7339eb9897d836f80
chronister <andrewLchronister@gmail.com>
2014-02-27 04:09:03 +0000

    Figure 3 and char change analysis

---
+++ Analysis/charchange.py
@@ -0,0 +1,28 @@
+import re
+import matplotlib.pyplot as plot
+
+def getFrequencies():
+    changevalues = []
+    infile = open("Frequency_results.tsv",'r')
+    line = infile.readline()
+    n = 0
+    while (line != "" and n < 5):
+        l = line.split("\t")
+        msg = l[1]
+        msg = list(msg)
+        msg = [ord(i) for i in msg]
+        enc = l[2]
+        hexes = enc.split(":")
+        hexes = [int(i, 16) for i in hexes]
+        for i in range(len(hexes) - 1):
+            changevalues.append(msg[i] - hexes[i])
+        line = infile.readline()
+    infile.close()
+    return changevalues
+
+changev = getFrequencies()
+plot.hist(changev, 256)
+plot.ylabel("Occurences")
+plot.xlabel("Displacement of character")
+plot.title("Displacement of Character Values")
+plot.show()
