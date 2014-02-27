972f82591adbe69a45bc9f996268cf60deaa3f74
chronister <andrewLchronister@gmail.com>
2014-02-27 05:28:06 +0000

    charchange data file

--- Analysis/charchange.py
+++ Analysis/charchange.py
@@ -1,8 +1,10 @@
 import re
 import matplotlib.pyplot as plot
+import numpy as np
 
 def getFrequencies():
     changevalues = []
+    frequencies = [[i, 0] for i in range(400)]
     infile = open("Frequency_results.tsv",'r')
     line = infile.readline()
     n = 0
@@ -15,14 +17,26 @@ def getFrequencies():
         hexes = enc.split(":")
         hexes = [int(i, 16) for i in hexes]
         for i in range(len(hexes) - 1):
-            changevalues.append(hexes[i] - msg[i])
+            change = hexes[i] - msg[i]
+            changevalues.append(change)
+            frequencies[change + 150][1] += 1
         line = infile.readline()
         #print("\n")
         #n += 1
     infile.close()
-    return changevalues
+    return changevalues, frequencies
 
-changev = getFrequencies()
+def writeFrequencies(frequencies):
+    outfile = open("charchange.txt",'w')
+    outfile.seek(0)
+    outfile.write(str([[i - 150, frequencies[i]] for i in range(len(frequencies))]))
+    outfile.write("\n")
+    outfile.write("\nMean: " + str(np.mean(frequencies)))
+    outfile.write("\nMedian: " + str(np.median(frequencies)))
+    outfile.close()
+
+changev, freq = getFrequencies()
+writeFrequencies([freq[i][1] for i in range(len(freq))])
 fig = plot.figure(3, (8, 5), 300)
 plot.hist(changev, 1024, color='black')
 plot.ylabel("Occurences")
